4a528a4a2b2c0483c83ebeef65f7a465fabb81dc
fitrahh18 <fitrahbahsri01@gmail.com>
2023-01-11 22:38:06 +0800

    Revert "Merge branch 'master' of https://github.com/fairunnasuha/Safely"
    This reverts commit 295b6d822a0f650f4ec072213db6b06ae046a075, reversing
    changes made to 28c8cabc69ca9ab78b0a413ba51e74621a71f7b9.

--- app/src/main/java/com/example/safely/MainActivity.java
+++ app/src/main/java/com/example/safely/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText passwordi;
 
-
     FirebaseAuth mAuth;
 
     @Override
--- app/src/main/java/com/example/safely/SecondActivity.java
+++ app/src/main/java/com/example/safely/SecondActivity.java
@@ -23,10 +23,6 @@ public class SecondActivity extends AppCompatActivity {
     TextView name,email;
     Button signOutBtn;
     FirebaseAuth mAuth;
-    Button button;
-    Button btnemergency;
-    Button btnprofile;
-    Button btnabout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +33,6 @@ public class SecondActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         signOutBtn = findViewById(R.id.signout);
         mAuth = FirebaseAuth.getInstance();
-        button =findViewById(R.id.guardian);
-        button.setOnClickListener(view -> {
-            startActivity(new Intent(SecondActivity.this,GuardianActivity.class));
-                });
-        btnemergency=findViewById(R.id.emergency);
-        btnemergency.setOnClickListener(view -> {
-            startActivity(new Intent(SecondActivity.this,sosActivity.class));
-        });
-        btnprofile=findViewById(R.id.profile);
-        btnprofile.setOnClickListener(view -> {
-            startActivity(new Intent(SecondActivity.this,ProfileActivity.class));
-        });
-        btnabout.findViewById(R.id.aboutus);
-        btnabout.setOnClickListener(view -> {
-            startActivity(new Intent(SecondActivity.this,AboutUs.class));
-        });
-
 
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
@@ -94,6 +73,5 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
     }
-
 
 }
