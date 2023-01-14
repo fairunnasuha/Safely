03ed931dd767321522110000d8940e041da9c1d6
fitrahh18 <fitrahbahsri01@gmail.com>
2023-01-14 22:01:33 +0800

    sos
    double tap and long press

--- app/src/main/java/com/example/safely/Location.java
+++ app/src/main/java/com/example/safely/Location.java
@@ -1,14 +1,26 @@
 package com.example.safely;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Location extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class Location extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
--- app/src/main/java/com/example/safely/sosActivity.java
+++ app/src/main/java/com/example/safely/sosActivity.java
@@ -3,12 +3,40 @@ package com.example.safely;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class sosActivity extends AppCompatActivity {
+    Button sosbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sos);
+
+        sosbtn = findViewById(R.id.sosbtn);
+        sosbtn.setOnTouchListener(new View.OnTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    Toast.makeText(getApplicationContext(),"Long press",Toast.LENGTH_SHORT).show();
+                    super.onLongPress(e);
+                }
+
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    Toast.makeText(getApplicationContext(),"Double Tap press",Toast.LENGTH_SHORT).show();
+                    return super.onDoubleTap(e);
+                }
+            });
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+               gestureDetector.onTouchEvent(motionEvent);
+                return false;
+            }
+        });
     }
 }
